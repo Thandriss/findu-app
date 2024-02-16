3ae484caec6696799746c28daf05d9a34ebca341
Thandriss <55065328+Thandriss@users.noreply.github.com>
2024-02-16 18:53:01 +0200

    Delete server/routes/config/keys.js

--- server/routes/config/keys.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    google: {
-        clientID: '494860623826-am1l9epe06a0kpk1148r6hupf2692gl6.apps.googleusercontent.com',
-        clientSecret: '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'
-    }
-};
